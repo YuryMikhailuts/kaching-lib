f4303e24fbab4f0238159c51d9757ba2b5819b17
Михайлуц Юрий Вячеславович <aracks@yandex.ru>
2024-04-24 02:08:37 +0300

    Переезд на kotlin 1.9.23

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,83 +3,84 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	apply {
-		kotlin("multiplatform") version "1.6.21"
-		`maven-publish`
-	}
+    apply {
+        kotlin("multiplatform") version "1.9.23"
+        `maven-publish`
+    }
 }
 
 group = "mikhaylutsyury"
-version = "1.0.2-SNAPSHOT"
+version = "1.1-SNAPSHOT"
 
 repositories {
-	mavenCentral()
+    mavenCentral()
+}
+
+tasks.wrapper {
+    gradleVersion = "8.7"
 }
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_11
-	targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 val commonCompileArgs = listOf(
-	"-opt-in=kotlin.RequiresOptIn",
-	"-Xuse-experimental=io.ktor.locations.KtorExperimentalLocationsAPI",
+    "-opt-in=kotlin.RequiresOptIn",
+    "-Xuse-experimental=io.ktor.locations.KtorExperimentalLocationsAPI",
 )
 tasks.withType(KotlinCompile::class).all {
-	kotlinOptions.freeCompilerArgs += commonCompileArgs
+    kotlinOptions.freeCompilerArgs += commonCompileArgs
 }
 kotlin {
-	jvm {
-		compilations.all {
-			kotlinOptions.jvmTarget = "11"
-			kotlinOptions.freeCompilerArgs += commonCompileArgs
-		}
-		withJava()
-		testRuns["test"].executionTask.configure {
-			useJUnitPlatform()
-		}
-	}
-	js(BOTH) {
-		compilations.all {
-			kotlinOptions.freeCompilerArgs += commonCompileArgs
-		}
-		browser {
-			testTask {
-				useMocha {
-					timeout = "30000"
-				}
-			}
-			commonWebpackConfig {
-				cssSupport.enabled = true
-			}
-		}
-	}
-	sourceSets {
-		val commonMain by getting {
-			dependencies {
-				implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
-				implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
-				implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.2")
-			}
-		}
-		val commonTest by getting {
-			dependencies {
-				implementation(kotlin("test"))
-				implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
-			}
-		}
-		val jvmMain by getting
-		val jvmTest by getting
-		val jsMain by getting {
-			dependencies {
-				implementation(npm("jstreemap", "1.28.2"))
-			}
-		}
-		val jsTest by getting
-	}
-	publishing {
-		publications {
-			all { }
-		}
-	}
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+            kotlinOptions.freeCompilerArgs += commonCompileArgs
+        }
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    js {
+        compilations.all {
+            kotlinOptions.freeCompilerArgs += commonCompileArgs
+        }
+        browser {
+            testTask {
+                useMocha {
+                    timeout = "30000"
+                }
+            }
+        }
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting
+        val jsMain by getting {
+            dependencies {
+                implementation(npm("jstreemap", "1.28.2"))
+            }
+        }
+        val jsTest by getting
+    }
+    publishing {
+        publications {
+            all { }
+        }
+    }
 }
